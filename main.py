2e01eb3f3b07165cb9342cba6bac5b5692759c15
TommehM <tomm.uk@me.com>
2013-03-27 09:57:35 +0000

    Major bug fixes

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import socket
 myNick  = "Bot"
 myIdent = "Bot"
 myReal  = "Bot"
-myIRC   = "irc.example.com"
-myPort  = "6667"
-myChan  = "#example" # only supports a single channel
-
+myIRC   = "irc.kottnet.net"
+myPort  = 6667
+myChan  = "#TomM" # only supports a single channel
 # Do not edit below this line
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect ( ( myIRC, myPort ) )
-socket.send( 'NICK ',myNick )
-socket.send( 'USER ',myIdent, myIdent, myIdent,' :',myReal,'\r\n' )
-socket.send( 'JOIN ',myChan )
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((myIRC, myPort))
+sock.send('NICK ' + myNick)
+sock.send('USER ' + myIdent + myIdent + myIdent + ' :' + myReal + '\r\n')
+sock.send('JOIN ' + myChan)
 # ok, now you can edit ^_^
 while True:
-        net = socket.recv ( 4096 )
-    if net.find ( 'PING' ) != -1:
-        socket.send ( 'PONG ' + net.split() [ 1 ] + '\r' )
-    if net.find ( 'cookie' ) != -1:
-        socket.send ( 'PRIVMSG ',myChan,' :mmmm cookies ;)\r\n' )
-print "Bot successfully connected to IRC"
+    server = sock.recv ( 4096 )
+    if server.find ( 'PING' ) != -1:
+        sock.send ( 'PONG ' + net.split() [ 1 ] + '\r' )
+    if server.find ( 'cookie' ) != -1:
+        sock.send ( 'PRIVMSG ' + myChan + ' :mmmm cookies ;)\r\n' )
